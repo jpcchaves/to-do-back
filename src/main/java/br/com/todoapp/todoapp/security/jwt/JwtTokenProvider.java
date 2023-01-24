01e65eeee2405b681b80f25dfa9e72b4270845b2
jpcchaves <joaopaulodecastrochaves@hotmail.com>
2023-01-24 14:23:31 -0300

    fix: resolve autowired injection bug UserDetailsService

--- src/main/java/br/com/todoapp/todoapp/security/jwt/JwtTokenProvider.java
+++ src/main/java/br/com/todoapp/todoapp/security/jwt/JwtTokenProvider.java
@@ -8,7 +8,6 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import jakarta.annotation.PostConstruct;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -25,13 +24,12 @@ import java.util.List;
 @Service
 public class JwtTokenProvider {
 
-    Algorithm algorithm = null;
     @Value("${security.jwt.token.expire-length:3600000}")
     private long validityInMilliseconds = 3600000; // 1h
     @Value("${security.jwt.token.secret-key:secret}")
     private String secretKey = "secret";
-    @Autowired
     private UserDetailsService userDetailsService;
+    Algorithm algorithm = null;
 
     @PostConstruct
     protected void init() {
